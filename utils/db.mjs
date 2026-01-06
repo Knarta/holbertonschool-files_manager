14f9dda7efef97b666324d4817aa5e66e8ca03cc
cableyesto <dialkins@tutamail.com>
2026-01-06 15:57:33 +0100

    feat: Create class abstraction for mongodb.

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,9 +1,9 @@
 import pkg from 'mongodb';
+
 const { MongoClient } = pkg;
 
 class DBClient {
   constructor() {
-
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
@@ -11,15 +11,15 @@ class DBClient {
     const url = `mongodb://${host}:${port}`;
     this.client = new MongoClient(url, { useUnifiedTopology: true });
 
-  this.dbName = database;
-  this.db = null;
-  this.client.connect()
-        .then(() => {
-          this.db = this.client.db(this.dbName);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
+    this.dbName = database;
+    this.db = null;
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(this.dbName);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   isAlive() {
@@ -31,11 +31,10 @@ class DBClient {
     return this.db.collection('users').countDocuments();
   }
 
-
-async nbFiles() {
-  if (!this.client.db()) return 0;
-   return this.db.collection('files').countDocuments();
-}
+  async nbFiles() {
+    if (!this.client.db()) return 0;
+    return this.db.collection('files').countDocuments();
+  }
 }
 const dbClient = new DBClient();
 export default dbClient;
